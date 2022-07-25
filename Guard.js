df5850f4f8bdbee6094b710e2bb3606fb4842bc1
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-07-24 21:22:03 -0400

    Update Guard.js

--- Guard.js
+++ Guard.js
@@ -7,64 +7,84 @@ export class Guard{
     }
 
     guard(v, v_indx, schema){
-            for(var i = 0; i<schema.length; i++){
-                try {
-                    this.nextGuard(v, v_indx,  schema[i])
-                    // if(this.terminate&&this.didTerminate){
-                    //     return
-                    // }
-                }catch(err){
-                }
+        for(var i = 0; i<schema.length; i++){
+            try {
+                this.nextGuard(v, v_indx,  schema[i])
+            }catch(err){
             }
+        }
     }
 
     nextGuard(v, v_indx, schema){
         if(this.isObjArray(schema)){
             this.guard(v, v_indx, schema)
         }else if(this.isObj(schema)){
-            if(this.isNKeys(schema, 1)){
-                //console.log("CALLING GUARD ON", v, v_indx, schema)
-                if(this.passGuard( v, v_indx, schema)[0]){
-                    this.nextGuard(v, v_indx+1, this.passGuard( v, v_indx, schema)[1])
-                }
-            }else{
-                throw Error( "Schema error, should never have more than 1 key to a non terminating level and should never have more than 2 keys to a terminating level")
-            }
+            this.passGuard(v, v_indx, schema)
         }else{
-            throw Error( 'schema must be of type object or of type array')
+            throw Error('schema must be of type object or of type array')
         }
     }
 
-    terminatingGuard(v, schema){
+    passGuard(v, v_indx, schema){
+        if(this.isNKeys(schema, 1)){
+            var passGuard = this._passGuard(v, v_indx, schema)
+            if(passGuard[0]){
+                schema = passGuard[1]
+                this.nextGuard(v, v_indx+1, schema)
+            }
+        }else{
+            throw Error(
+                `Schema error, should never have more than 1 key 
+                to a non terminating level and should never have 
+                more than 2 keys to a terminating level`
+            )
+        }
+    }
+
+    _passGuard(v, v_indx, schema){
+        if(this.isTerminatingGuard(schema)){
+            this.terminate(v, schema)
+        }else{
+            return [this.callGuard( Object.keys(schema)[0], v[v_indx]) , schema[Object.keys(schema)[0]]]
+        }
+    }
+
+    callGuard(func, _v){
+        try{
+            //this needs to be fixed for types that are not strings
+            func='this.'+func
+            func = this.buildParams(func, _v)
+
+            if(eval(func)){
+                return true
+            }else{
+                return false
+            }
+        }catch{
+            throw Error("Cannot Call Guard function, Check Schema")
+        }
+    }
+
+    terminate(v, schema){
         this.didTerminate=true
         if(this.isString(schema[Object.keys(schema)[0]])){
         }else{
             if(this.callGuard(Object.keys(schema)[0], v[v.length-1])){
-                this.buildTerminator(v, schema, false)
+                this._terminate(v, schema, false)
             }else{
-                this.buildTerminator(v, schema, true)
+                this._terminate(v, schema, true)
             }
         }
     }  
 
-    buildTerminator(v, schema, deflt){
-        var func = schema[Object.keys(schema)[0]]['FUNCTION']
-        func='this.obj.'+func+'('
-
+    _terminate(v, schema, deflt){
         if(deflt){
             v.pop()
             v.push(schema[Object.keys(schema)[0]]['DEFAULT'])
         }
-        v.forEach((_v)=>{
-            if(this.isString(_v)){
-                func+="'"+_v+"'"+','
-            }else{
-                func+=_v+','
-            }
-        })
-        func = func.substring(0, func.length-1)
-        func+=')'
-        console.log(func)
+        var func = schema[Object.keys(schema)[0]]['FUNCTION']
+        func='this.obj.'+func
+        func=this.buildParams(func, v)
         eval(func)
     }
 
@@ -81,30 +101,19 @@ export class Guard{
             }
         }
         return false
-    }
+    }    
 
-    passGuard(v, v_indx, schema){
-        if(this.isTerminatingGuard(schema)){
-            this.terminatingGuard(v, schema)
-        }else{
-            return [this.callGuard( Object.keys(schema)[0], v[v_indx]) , schema[Object.keys(schema)[0]]]
-        }
-    }
-
-    callGuard(func, _v){
-        try{
-            //this needs to be fixed for types that are not strings
-            func+='('+'"' +_v + '"' + ')'
-            func='this.'+func
-            console.log(func)
-            if(eval(func)){
-
-                return true
+    buildParams(func, v){
+        func+='('
+        v.forEach((_v)=>{
+            if(this.isString(_v)){
+                func+="'"+_v+"'"+','
             }else{
-                return false
+                func+=_v+','
             }
-        }catch{
-            throw Error( "Cannot Call Guard function, Check Schema")
-        }
+        })
+        func = func.substring(0, func.length-1)
+        func+=')'
+        return func
     }
 }
