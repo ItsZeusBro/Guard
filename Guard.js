4f06fc1cf8568ce3e6d023e21d7a575045d356f0
Zeus <zeusofolympus1000@gmail.com>
2022-07-25 12:44:41 -0400

    error messages

--- Guard.js
+++ Guard.js
@@ -15,28 +15,22 @@ export class Guard{
                         this.guard(v, v_indx, schema_obj)
                     }else if(this.g.isObj(schema_obj)){
                         if(this.g.isNKeys(schema_obj, 1)){
-                            //it has to be a guard or we throw an error
                             var func = Object.keys(schema_obj)[0]
                             var val = schema_obj[func]
                             if(this.g.isGuard(func)){
-                                //attempt to pass guard
                                 if(this.g.passGuard(func, v[v_indx])){
-                                    //if we pass the guard, we need to check if it terminates
-                                    //on a string or object, then call terminate
                                     if(this.isTerminal(val)=='string'){
                                         return
                                     }else if(this.isTerminal(val)=='obj'){
                                         return
                                     }else{
-                                        console.log(v[v_indx], "passed guard", func, "but is not terminal")
+                                        console.log(v[v_indx], "passed guard", func, "but is not terminal, must recurse")
                                         this.guard(v, v_indx+1, schema_obj)
                                     }
-                                }else{
-                                    throw Error(v[v_indx], "did not pass guard", func)
-                                }
+                                }else{throw Error(v[v_indx], "did not pass guard", func)}
                             }else{throw Error("schema error:", schema_obj, "is not a guard object")}
-                        }else{throw Error(`Schema Error, multiple guard keys at same level`)}
-                    }else{throw Error('Schema Error: must be an object if its not an array')}
+                        }else{throw Error("Schema Error, multiple guard keys", Object.keys(schema_obj), "at same level")}
+                    }else{throw Error("Schema Error:",schema_obj, "must be an object if its not an array but is not")}
             }catch(err){}
         })
     }
