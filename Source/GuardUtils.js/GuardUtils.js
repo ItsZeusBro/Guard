54f843215b665a0083f589a7b8508259401e34b0
Zeus <zeusofolympus1000@gmail.com>
2022-07-28 17:54:01 -0400

    exporting utils and util tests

---
+++ Source/GuardUtils.js/GuardUtils.js
@@ -0,0 +1,214 @@
+export class GuardUtils{
+    constructor(guardFuncBag){
+        this.guardFuncBag=guardFuncBag
+        this.guards = new Guards()
+        this.rg = new RandGen()
+        this.gfBag = guardFuncBag
+    }
+
+    log(obj){
+        if(obj){
+            console.log(util.inspect(obj, false, null, true))
+        }
+    }
+    walk(guard){
+        if(!this.guards.isArr(guard)){throw Error('guard schema has no base array')}
+        var arr = []
+
+        return arr
+    }
+
+    _walk(guard, arr){
+        if(!this.guards.isArr(guard)){throw Error('guard schema has no base array')}
+        
+        return arr
+    }
+
+    getGuardAtIndex(guard, indx){
+
+    }
+
+    isRecursiveTypeBlockObj(guardObj){
+        if((this.guardFuncBag.includes(Object.keys(guardObj)[0])== true) && Object.keys(guardObj).length==1){
+            return true
+        }
+    }
+
+    isRecursiveDefaultBlockObj(guardObj){
+        var defaultPresent;
+        var recursivePresent;
+        for(var i = 0; i<Object.keys(guardObj).length; i++){
+            if(this.guardFuncBag.includes(Object.keys(guardObj)[i])){recursivePresent=true}
+        }
+        if(Object.keys(guardObj).includes('~DEFAULT~')){defaultPresent=true}
+        if(defaultPresent&&recursivePresent){
+            return true
+        }
+    }
+
+    isTerminalTypeBlockObj(guardObj){
+        if(Object.keys(guardObj).length==1){
+            var arr = guardObj[Object.keys(guardObj)[0]].split('is')
+            var guard = 'is'+arr.pop()
+            if(
+                this.guardFuncBag.includes(Object.keys(guardObj)[0])
+                &&
+                this.guardFuncBag.includes(guard)
+            ){
+                return true
+            }
+        }
+    }
+
+    isTerminalDefaultBlockObj(guardObj){
+        if(Object.keys(guardObj).length==2){
+            var defaultPresent;
+            var recursivePresent;
+            var recursiveIndex;
+            for(var i = 0; i<Object.keys(guardObj).length; i++){
+                try{
+                    var arr = guardObj[Object.keys(guardObj)[i]].split('is')
+                    var guard = 'is'+arr.pop()
+                    if(this.guardFuncBag.includes(guard)){
+                        recursivePresent=true
+                        recursiveIndex=i
+                    }
+                }catch{
+                    
+                }
+
+            }
+            if(Object.keys(guardObj).includes('~DEFAULT~')){defaultPresent=true}
+
+            var arr = guardObj[Object.keys(guardObj)[recursiveIndex]].split('is')
+            var guard = 'is'+arr.pop()
+            if(
+                this.guardFuncBag.includes(Object.keys(guardObj)[recursiveIndex])
+                &&
+                this.guardFuncBag.includes(guard)
+                &&
+                defaultPresent
+            ){
+                return true
+            }
+        }
+    }
+
+    newGuardFunc(){
+        return this.rg.randSelection(this.gfBag)
+    }
+
+    newRecursiveTypeBlockObj(guardFuncStr){
+        if(!guardFuncStr){
+            var newGuardFuncStr = this.newGuardFunc()
+            return {
+                [newGuardFuncStr]:[]
+            }
+        }else{
+            var newGuardFuncStr = this.newGuardFunc()
+            guardFuncStr+=newGuardFuncStr
+            return {
+                [newGuardFuncStr]:[]
+            }
+        }
+    } 
+
+    newRecursiveDefaultBlockObj(guardFuncStr){
+        if(!guardFuncStr){
+
+            var newGuardFuncStr = this.newGuardFunc()
+
+            return {
+                '~DEFAULT~':this.defaultVal(newGuardFuncStr),
+                [newGuardFuncStr]:[]
+            }
+
+        }else{
+
+            var newGuardFuncStr = this.newGuardFunc()
+            guardFuncStr+=newGuardFuncStr
+
+            return {
+                '~DEFAULT~':this.defaultVal(newGuardFuncStr),
+                [newGuardFuncStr]:[]
+            }
+        }
+    }
+
+    newTerminalTypeBlockObj(guardFuncStr){
+        if(!guardFuncStr){
+            var newGuardFuncStr = this.newGuardFunc()
+            return {
+                [newGuardFuncStr]:newGuardFuncStr
+            }
+        }else{
+            var newGuardFuncStr = this.newGuardFunc()
+            guardFuncStr+=newGuardFuncStr
+            return {
+                [newGuardFuncStr]:guardFuncStr
+            }
+        }
+    }
+
+    newTerminalDefaultBlockObj(guardFuncStr){
+        if(!guardFuncStr){
+            var newGuardFuncStr = this.newGuardFunc()
+            var defaultVal=this.defaultVal(newGuardFuncStr)
+            return {
+                '~DEFAULT~':defaultVal,
+                [newGuardFuncStr]:newGuardFuncStr
+            }
+        }else{
+            var newGuardFuncStr = this.newGuardFunc()
+            var defaultVal=this.defaultVal(newGuardFuncStr)
+            guardFuncStr+=newGuardFuncStr
+            return {
+                '~DEFAULT~':defaultVal,
+                [newGuardFuncStr]:guardFuncStr
+            }
+        }
+    }
+
+    
+    defaultVal(guardFunc){
+        //generate a random value with the type in question and return it
+        if(guardFunc=='isStr'){
+            return this.rg.randStr()
+        }else if(guardFunc=='isInt'){
+            return this.rg.randInt()
+        }else if(guardFunc=='isArr'){
+            return this.rg.randArr()
+        }else if(guardFunc=='isIntArr'){
+            return this.rg.randIntArr()
+        }else if(guardFunc=='isEnc'){
+            return this.rg.randEnc()
+        }else if(guardFunc=='isEncArr'){
+            return this.rg.randEncArr()
+        }else if(guardFunc=='isStrArr'){
+            return this.rg.randStrArr()
+        }else if(guardFunc=='isObj'){
+            return this.rg.randObj()
+        }else if(guardFunc=='isObjArr'){
+            return this.rg.randObjArr()
+        }
+    }
+    testGuard(){
+        return (test_case, guard, func, expectedResult)=>{
+            eval(
+                `class TestGen{
+                    constructor(test_case, guard, expected_result){
+                        this.expectedResult=expected_result
+                        new Guard(new Guards(), test_case, guard,  this)
+                        //console.log(${func})
+                    }
+                    ${func}(v){
+                        assert.deepEqual(v, this.expectedResult[1])
+                        console.log(func+"("+ JSON.stringify(this.expectedResult[1])+')', 'PASSES')
+                    }
+                } 
+                new TestGen(${test_case}, ${guard}, ${expectedResult})
+                `
+            )
+        }
+    }
+}
---
+++ Source/GuardUtils.js/GuardUtilsTest.js
@@ -0,0 +1,78 @@
+export class TestUtils{
+    constructor(guardFuncBag){
+        this.guardFuncBag=guardFuncBag
+        this.gu = new GuardUtils(guardFuncBag)
+        this.tests()
+    }
+
+    tests(){
+        this.defaultValueTestAndAllBlockTests()
+    }
+
+    defaultValueTestAndAllBlockTests(){
+        this.testNewRecursiveTypeBlockObj()
+        this.testNewRecursiveDefaultBlockObj()
+        this.testNewTerminalTypeBlockObj()
+        this.testTerminalDefaultBlockObj()
+    }
+
+    testIsRecursiveTypeBlockObj(){
+        var obj = this.gu.newRecursiveTypeBlockObj();
+        assert.equal(this.gu.isRecursiveTypeBlockObj(obj), true);
+        return true
+    }
+
+    testIsRecursiveDefaultBlockObj(){
+        var obj = this.gu.newRecursiveDefaultBlockObj();
+        assert.equal(this.gu.isRecursiveDefaultBlockObj(obj), true);
+        return true
+    }
+
+    testIsTerminalTypeBlockObj(){
+        var obj = this.gu.newTerminalTypeBlockObj()
+        assert.equal(this.gu.isTerminalTypeBlockObj(obj), true);
+        return true
+    }
+
+    testIsTerminalDefaultBlockObj(){
+        var obj = this.gu.newTerminalDefaultBlockObj()
+        assert.equal(this.gu.isTerminalDefaultBlockObj(obj), true);
+        return true
+    }
+
+    testNewRecursiveTypeBlockObj(){
+        for(var i=0; i<10000; i++){
+            assert.equal(this.testIsRecursiveTypeBlockObj(), true)
+        }
+        return true
+    }
+
+    testNewRecursiveDefaultBlockObj(){
+        for(var i=0; i<10000; i++){
+            assert.equal(this.testIsRecursiveDefaultBlockObj(), true)
+        }
+        return true
+    }
+
+    testNewTerminalTypeBlockObj(){
+        for(var i=0; i<10000; i++){
+            assert.equal(this.testIsTerminalTypeBlockObj(), true)
+        }
+        return true
+    }
+
+    testTerminalDefaultBlockObj(){
+        for(var i=0; i<10000; i++){
+            assert.equal(this.testIsTerminalDefaultBlockObj(), true)
+        }
+        return true
+    }
+
+    testNewGuardFunc(){
+
+    }
+
+    testGetGuardAtIndex(guard, indx){
+
+    }
+}
