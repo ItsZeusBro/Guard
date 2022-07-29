235ef7b012ce02bb768e7f2c338d2684de16685b
Zeus <zeusofolympus1000@gmail.com>
2022-07-29 00:55:19 -0400

    we need a lookahead before default feature

--- Guard.js
+++ Guard.js
@@ -1,11 +1,13 @@
+import { GuardUtils } from "./Source/GuardUtils.js/GuardUtils";
 export class Guard{
+
     constructor(guards, v, schema, obj){
         this.obj=obj
         this.didTerminate=false
         this.g = guards
         this.q=[];
         //console.log(guards, v, schema, obj)
-
+        this.gu = GuardUtils(['isStr', 'isInt', 'isArr', 'isIntArr', 'isEnc', 'isEncArr', 'isStrArr', 'isObj', 'isObjArr'])
         this.guard(v, 0, schema)
 		//if the last item pushes to the queue, did not throw an error, flush the queue
 		if(!this.didTerminate){
@@ -25,37 +27,6 @@ export class Guard{
             }
         }
         return
-    }
-
-
-
-    isTerminal(v, v_indx, val){
-
-        if(this.g.isStr(val)&&(v_indx==v.length-1)){
-
-            return 'string';
-        }else if(this.g.isObj(val)&&(!this.g.isArr(val))&&(v_indx==v.length-1)){
-            //deep check required!
-            if(this.g.isKey(val,'DEFAULT')&&this.g.isKey(val, 'FUNCTION')){
-                return 'obj';
-            }
-        }
-    }
-
-    terminatingObj(func, v, v_indx, obj){
-        if(this.g.passGuard(func, v[v_indx])){
-            this.didTerminate=true
-            func = this.g.buildParams(obj['FUNCTION'], v)
-            
-            eval('this.obj.'+func)
-
-        }else{
-            var _v= Array.from(v);
-            _v.pop()
-            _v.push(obj['DEFAULT'])
-            func = this.g.buildParams(obj['FUNCTION'], _v)
-            this.q.push('this.obj.'+func)
-        } 
     }
 
     terminatingStr(v, str){
