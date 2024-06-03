424a79e134896d131ebfc486b8f25f102da7190a
Zeus <zeusofolympus1000@gmail.com>
2024-06-03 17:49:31 -0400

    saving

--- Source/Utils/Terminal.js
+++
@@ -1,8 +0,0 @@
-class Terminal{
-    constructor(thing){
-        this.thing = this.interpret(thing)
-    }
-    interpret(){
-        //should return a terminal object that we can rely on to be the same for all cases 
-    }
-}
